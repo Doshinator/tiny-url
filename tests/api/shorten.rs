7dee66d7368f1d7317ecf08f59a68b8ab5cd0892
Rushabh Doshi <rush3doshi@gmail.com>
2026-05-30 22:33:47 -0400

    shorten test completed

--- tests/api/shorten.rs
+++ tests/api/shorten.rs
@@ -6,9 +6,10 @@ async fn shorten_returns_201_for_valid_url() {
     // Arrange
     let app = spawn_app().await;
     let client = reqwest::Client::new();
+    let url = format!("{}/shorten", app.address);
 
     let response = client
-        .post(format!("{}/shorten", app.address))
+        .post(url)
         .json(&serde_json::json!({
             "long_url": "https://www.google.com"
         }))
@@ -21,4 +22,54 @@ async fn shorten_returns_201_for_valid_url() {
     let body: serde_json::Value = response.json().await.unwrap();
     assert!(body["short_code"].as_str().is_some());
     assert!(body["short_url"].as_str().is_some());
+}
+
+#[tokio::test]
+async fn shorten_returns_400_bad_request() {
+    // Arrange
+    let app = spawn_app().await;
+    let client = reqwest::Client::new();
+    let url = format!("{}/shorten", app.address);
+    
+    let response = client
+        .post(url)
+        .json(&serde_json::json!({
+            "long_url": "Some invalid url"
+        }))
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    assert_eq!(response.status().as_u16(), 400);
+}
+
+#[tokio::test]
+async fn shorten_returns_409_alias_taken() {
+    // Arrange
+    let app = spawn_app().await;
+    let client = reqwest::Client::new();
+    let url = format!("{}/shorten", app.address);
+
+    // first request — claim the alias
+    client
+        .post(&url)
+        .json(&serde_json::json!({
+            "long_url": "https://www.google.com",
+            "alias": "my-alias"
+        }))
+        .send()
+        .await
+        .expect("Failed to execute first request");
+
+    let response = client
+        .post(url)
+        .json(&serde_json::json!({
+            "long_url": "https://www.google.com",
+            "alias": "my-alias"
+        }))
+        .send()
+        .await
+        .expect("Failed to execute second request");
+
+    assert_eq!(response.status().as_u16(), 409);
 }
