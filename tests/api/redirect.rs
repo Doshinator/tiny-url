11179e9051b852fc36f137ff5bd238f9a30fdf9d
Rushabh Doshi <rush3doshi@gmail.com>
2026-06-01 22:27:52 -0400

    test redirect::redirect_returns_400_invalid_short_code ... ok test redirect::redirect_404_short_code_doesnt_exist ... ok test redirect::redirect_410_expired_short_code ... ok test redirect::successful_redirect_302 ... ok

--- tests/api/redirect.rs
+++ tests/api/redirect.rs
@@ -30,5 +30,75 @@ async fn successful_redirect_302() {
         .await
         .expect("Failed to execute GET request");
 
+    // Assert
     assert_eq!(response.status().as_u16(), 302);
+    assert_eq!(
+        response.headers().get("Location").unwrap(),
+        "https://www.google.com"
+    );
+}
+
+#[tokio::test]
+async fn redirect_returns_400_invalid_short_code() {
+    // Arrange
+    let app = spawn_app().await;
+    let client = reqwest::Client::new();
+    let short_code = "bad$code";
+
+    // Act
+    let response = client
+        .get(format!("{}/{}", app.address, short_code))
+        .send()
+        .await
+        .expect("Failedto execute GET request");
+
+    // Assert
+    assert_eq!(response.status().as_u16(), 400);
+}
+
+#[tokio::test]
+async fn redirect_404_short_code_doesnt_exist() {
+    // Arrange
+    let app = spawn_app().await;
+    let client = reqwest::Client::new();
+    let short_code = "inexisting-short-code";
+
+    // Act
+    let response = client
+        .get(format!("{}/{}", app.address, short_code))
+        .send()
+        .await
+        .expect("Failed to execute GET request");
+
+    // Assert
+    assert_eq!(response.status().as_u16(), 404);   
+}
+
+#[tokio::test]
+async fn redirect_410_expired_short_code() {
+    // Arrange
+    let app = spawn_app().await;
+    let client = reqwest::Client::new();
+    let short_code = "expired";
+    let long_url = "https://www.google.com";
+
+    sqlx::query!(
+        "INSERT INTO urls (id, short_code, long_url, created_at, expires_at)
+        VALUES ($1, $2, $3, now(), now() - interval '1 day')",
+        uuid::Uuid::new_v4(),
+        short_code,
+        long_url,
+    )
+    .execute(&app.db_pool)
+    .await
+    .unwrap();
+
+    let response = client
+        .get(format!("{}/{}", app.address, short_code))
+        .send()
+        .await
+        .expect("Failed to execute GET request");
+
+    // Assert
+    assert_eq!(response.status().as_u16(), 410);      
 }
